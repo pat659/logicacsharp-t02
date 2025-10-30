eb36770cfb0e0ce219a3d68a6ec28c2900f29774
PATRICIA MARTINS DE SOUZA <patricia.msouza48@senacsp.edu.br>
2025-10-30 17:06:08 -0300

    first commit

---
+++ variaveiseconstantes/Program.cs
@@ -0,0 +1,43 @@
+﻿//Variáveis
+char letra = 'A';
+string nome = "João";
+float temperatura = 36.5f;
+double salario = 12345.67;
+bool estaChovendo = true;
+
+// Alternando as variáveis
+letra = 'B';
+nome = "Maria";
+temperatura = 37.0f;
+salario = 15000.50;
+estaChovendo = false;
+
+//Constantes
+const char simbolo = '$';
+const string saudacao = "Bem-vindo a C#";
+const float pi = 3.14159f;
+const double altura = 1.75;
+const bool ehVerdade = false;
+const int numero = 14;
+
+// Exibindo os valores originais por interpolação
+Console.WriteLine("Valores originais:");
+Console.WriteLine($"Letra: {letra}");
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Temperatura: {temperatura}");
+Console.WriteLine($"Salário: {salario}");
+Console.WriteLine($"Está chovendo? {estaChovendo}");
+
+
+//Exibindo valores originais por concatenação
+Console.WriteLine("\nConsatantes:");
+Console.WriteLine("Simbolo: " + simbolo);
+Console.WriteLine("Saudação: " + saudacao);
+Console.WriteLine("Valor de pi: " + pi);
+Console.WriteLine("Altura: " + altura);
+Console.WriteLine("É verdade? " + ehVerdade);
+Console.WriteLine("Número: " + numero);
+
+
+
+
