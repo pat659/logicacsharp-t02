c09d8be71a7bece24811944391dff0b4644b5410
PATRICIA MARTINS DE SOUZA <patricia.msouza48@senacsp.edu.br>
2025-11-11 16:46:42 -0300

    condicionais

---
+++ condicionais/Program.cs
@@ -0,0 +1,101 @@
+﻿// Condicional IF
+using System;
+using System.ComponentModel.Design;
+
+string nome = "Ana";
+
+if(nome == "Ana")
+{
+    Console.WriteLine("Olá, Ana!");
+}
+
+string texto = "";
+
+if (string.IsNullOrEmpty(texto))
+    Console.WriteLine("A string está vazia.");
+
+// Condicional IF/ELSE
+int idade = 16;
+
+if (idade >= 18)
+{
+    Console.WriteLine("Você é maior de idade.");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade.");
+}
+
+int numero = 7;
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("O número é par.");
+}
+else
+{
+    Console.WriteLine("O número é impar.");
+}
+
+// Condicional usando else if
+double nota = 6.5;
+
+if (nota >= 9.0)
+{
+    Console.WriteLine("Você obteve uma excelente nota! Aprovado com distinção.");
+}
+else if (nota >= 7.0)
+    {
+        Console.WriteLine("Você está aprovado.");
+    }
+else if (nota >= 5.0)
+{
+    Console.WriteLine("Você está em recuperação");
+}
+else if (nota >= 3.0)
+{
+    Console.WriteLine("Você está em risco de recuperação");
+}
+else
+{
+    Console.WriteLine("Você foi reprovado.");
+}
+
+//Condicioal switch
+double pontuacao = 8.5;
+
+// Arredondando a nota para o inteiro mais próximo
+int faixaNota = (int)Math.Floor(pontuacao);
+
+switch (faixaNota)
+{
+    case 10:
+        Console.WriteLine("Você obteve uma excelente nota! Aprovado com distinção.");
+        break;
+    case 9:
+    case 8:
+        Console.WriteLine("Você está aprovado.");
+        break;
+    case 7:
+    case 6:
+        Console.WriteLine("Você está em recuperação");
+        break;
+    case 5:
+    case 4:
+    case 3:
+        Console.WriteLine("Você está em risco de recuperação");
+        break;
+    default:
+        Console.WriteLine("Você foi reprovado!");
+        break;
+}
+
+ 
+
+
+
+
+
+
+
+
---
+++ operadoresrelacionais/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 10;
+int b = 5;
+
+//Operadores Relacionais 
+bool maiorQUE = a > b;
+bool menorQUE = a < b;
+bool igual = a == b;
+bool diferente = a != b;
+bool maiorOuIgual = a >= b;
+bool menorOuigual = a <= b;
+
+//Exibindo resultados dos operadores relacionais
+Console.WriteLine("\nOperadores Relacionais:");
+Console.WriteLine($"{a} > {b}: {maiorQUE}");
+Console.WriteLine($"{a} < {b}: {menorQUE}");
+Console.WriteLine($"{a} == {b}: {igual}");
+Console.WriteLine($"{a} != {b}: {diferente}");
+Console.WriteLine($"{a} >= {b}: {maiorOuIgual}");
+Console.WriteLine($"{a} <= {b}: {menorOuigual}");
+
