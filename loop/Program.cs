05ba9781c96198d0fae948b1dc9f6b62a6a5319d
PATRICIA MARTINS DE SOUZA <patricia.msouza48@senacsp.edu.br>
2025-11-13 16:35:55 -0300

    aula 13-11

---
+++ loop/Program.cs
@@ -0,0 +1,65 @@
+﻿//loop for 
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+int soma = 0;
+for (int i = 1; i <= 100; i++)
+{
+    soma += i; // Soma o valor de i à variável soma, exemplo: 1+2+3+4...+99+100
+}
+
+Console.WriteLine("A soma dos números de 1 a 100 é:" + soma);
+
+//loop infinito
+/*for (;;)
+{
+    Console.WriteLine("Este é um loop infinito");
+}
+*/
+
+//Loop While (enquanto)
+int contador = 1;
+
+while (contador <= 5)
+{
+    Console.WriteLine(contador);
+    contador++; // Incrementa o contador
+}
+
+// Loop Do While (faça enquanto)
+int novocontador = 2;
+
+do
+{
+    Console.WriteLine(novocontador);
+    novocontador++; // Incrementa o contador
+} while (contador <= 5);
+
+//Loop Infinito While 
+/*while(true)
+{
+    Console.WriteLine("Este é um loop infinito!");
+}
+*/
+
+// Loop Foreach
+
+// Uma lista de strings 
+List<string> nomes = new List<string> { "Ana", "Carlos", "João", "Maria" };
+
+//Usando o foreach para percorrer a lista 
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome); // Imprime cada nome exibindo na tela
+}
+
+//Um vetor de números nteiros
+int[] numeros = { 1, 2, 3, 4, 5 };
+
+//Usando o foreach para percorrer o vetor 
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero); // Imprime cada número exibindo na tela
+}
